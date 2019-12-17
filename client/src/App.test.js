7e3e690cb458d3b7b2243425d6c1e626c3ac1641
N Wharton <wharton.n@gmail.com>
2019-12-17 12:15:28 +0100

    Initial Commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+	// commenting out test b/c app won't be ready on run
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
